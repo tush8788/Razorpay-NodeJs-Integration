e77e1d2df995065165e6ab131b274ca23fe01ab5
tush87 <tusharj8788@gmail.com>
2023-03-22 14:10:29 +0530

    remove unuse code

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const port = 8000;
-// const razorpay = require('razorpay');
-// const razorpayFun = require('./config/razorpay');
-// const crypto=require('crypto')
 
 const app = express();
 
@@ -10,15 +7,18 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./view");
 
+//set static 
 app.use(express.static('./assets'));
 app.use(express.urlencoded({ extended: false }))
 
 // url handler
 app.use('/',require('./routes/index'));
 
+//listen
 app.listen(port, function (err) {
     if (err) {
-
+        console.log(err);
+        return;
     }
     console.log(`server up on port ${port}`);
 })
